bbc808ee12fe71f907308d238741072aa7680099
Victoria <tapia.victoria5070@gmail.com>
2016-03-03 18:38:31 -0800

    added Home Component

---
+++ app/components/Home.js
@@ -0,0 +1,31 @@
+var React = require('react');
+
+var Home = React.createClass({
+  getInitialState: function(){
+    return {
+      notes: [],
+      bio: {
+        name: ''
+      },
+      repos: []
+    }
+  },
+  render: function(){
+    console.log(this.props)
+    return (
+      <div className="row">
+        <div className="col-md-4">
+          Shows Image Crousel
+        </div>
+        <div className="col-md-4">
+          Show Titles Navbar
+        </div>
+        <div className="col-md-4">
+          Show Thumbnails and descriptions
+        </div>
+      </div>
+    )
+  }
+})
+
+module.exports = Home;
