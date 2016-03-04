df1fbd2f48980e42a0a29a7ab72bd0baf616d2c2
Victoria <tapia.victoria5070@gmail.com>
2016-03-03 22:14:45 -0800

    added carousel and shows nav bar components for home page

---
+++ app/components/Carousel.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Carousel = React.createClass({
+  render: function () {
+    return (
+      <div>Bootstrap Carousel With Show Images Here</div>
+    )
+  }
+})
+
+module.exports = Carousel;
---
+++ app/components/ShowsNav.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var ShowsNav = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Nav Bar With Show Titles Here
+      </div>
+    )
+  }
+})
+
+module.exports = ShowsNav;
